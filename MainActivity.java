fffeb7e2625ffcac3f6fc082435a8196f6693e0e
JackC <jcrymble02@qub.ac.uk>
2017-03-09 23:35:13 +0000

    ScreenDimensions used to access the height and width of the total screen, and also the height and width of the playable screen (table)

---
+++ MainActivity.java
@@ -0,0 +1,68 @@
+package development.crymble.jack.poolsimulator;
+
+/**
+ * Created by jackc on 26/02/2017.
+ */
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        //
+        ScreenDimensions.setScreen_height(height);
+        ScreenDimensions.setScreen_width(width);
+
+        setContentView(new GameView(this));
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    // This method executes when the player quits the game
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+}
---
+++ MainThread.java
@@ -0,0 +1,83 @@
+package development.crymble.jack.poolsimulator;
+
+import android.graphics.Canvas;
+import android.provider.Settings;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by jackc on 09/03/2017.
+ */
+
+public class MainThread extends Thread {
+
+    public static final int MAX_FPS = 30;
+    private double averageFPS;
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private boolean running;
+    public static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GameView gameView){
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+
+    @Override
+    public void run(){
+        long startTime;
+        long timeMillis = 1000/MAX_FPS;
+        long waitTime;
+        long frameCount = 0;
+        long totalTime = 0;
+        long targetTime = 1000/MAX_FPS;
+
+        while(running){
+            startTime = System.nanoTime();
+            canvas = null;
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+
+                synchronized (surfaceHolder){
+                    this.gameView.update();
+                    this.gameView.draw(canvas);
+                }
+
+            } catch(Exception e){
+                e.printStackTrace();
+            } finally {
+                if(canvas != null){
+                    try{
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+            timeMillis = (System.nanoTime() - startTime/1000000);
+            waitTime = targetTime - timeMillis;
+            try{
+                if(waitTime > 0){
+                    this.sleep(waitTime);
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if(frameCount == MAX_FPS){
+                averageFPS = 1000/((totalTime/frameCount)/1000000);
+                frameCount = 0;
+                totalTime = 0;
+                System.out.print(averageFPS);
+            }
+        }
+    }
+
+    public void setRunning(boolean running){
+        this.running = running;
+    }
+
+}
